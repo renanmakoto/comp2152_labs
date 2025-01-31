4b60ce8b76cc5c992f6175eb43058d70a7d106cd
renan <renanmakoto@outlook.com>
2025-01-31 11:32:39 -0500

    Q5

--- Week04/lab_week4.py
+++ Week04/lab_week4.py
@@ -163,6 +163,15 @@ loot = loot_options .pop(lootRoll - 1)
 belt.append(loot)
 print(f"Your belt: {belt}")
 
+#Lab04 - Q5 - Item 2
+print("You found a loot bag! Look inside to find 2 items")
+input("Roll for the second item (Press Enter)")
+lootRoll = random.choice(range(1, len(loot_options) + 1))
+loot = loot_options .pop(lootRoll - 1)
+belt.append(loot)
+print(f"Your belt: {belt}")
+
+
 input("Analyze the roll (Press enter)")
 # Compare Player vs Monster's strength
 print("--- You are matched in strength: " + str(combat_strength == m_combat_strength))

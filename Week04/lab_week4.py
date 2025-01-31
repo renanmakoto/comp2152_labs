c05dd2bb89e95a43584f77687f15a7fbb102bc92
renan <renanmakoto@outlook.com>
2025-01-31 11:40:59 -0500

    Q8

--- Week04/lab_week4.py
+++ Week04/lab_week4.py
@@ -175,6 +175,20 @@ print(f"Your belt: {belt}")
 belt.sort()
 print(f"Your belt items sorted as follow: {belt}")
 
+#Lab04 - Q8 - Use the belt
+print("You saw a monster! Quickly use your first item.")
+first_item = belt.pop(0)
+if first_item in good_loot_options:
+    health_points = min(6, (health_points + 2))
+    print(f"You used the {first_item} and your health changed to {health_points}")
+elif first_item in bad_loot_options:
+    health_points = max(0, (health_points - 2))
+    print(f"You used the {first_item} and your health changed to {health_points}")
+else:
+    print(f"You used the {first_item}, but it was not helpful")
+
+
+
 
 input("Analyze the roll (Press enter)")
 # Compare Player vs Monster's strength

e145b3466c1e3aceaac3ec40c6d8cd4d6ff63d49
renan <renanmakoto@outlook.com>
2025-01-31 11:21:49 -0500

    Q4

--- Week04/lab_week4.py
+++ Week04/lab_week4.py
@@ -81,6 +81,9 @@ loot_options = ["Health Potion", "Poison Potion", "Secret Note", "Leather Boots"
 good_loot_options = ["Health Potion", "Leather Boots"]
 bad_loot_options = ["Poison Potion"]
 
+#Lab04 - Q4
+belt = []
+
 #Lab04 - Q1
 # Define the Monster Power:
 monster_power = {

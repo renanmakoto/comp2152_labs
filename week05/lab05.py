c0e89f0d63a4f2c59d90a492f5bfc79abddd6f85
renan <renanmakoto@outlook.com>
2025-02-07 10:32:15 -0500

    week05

--- week05/lab05.py
+++ week05/lab05.py
@@ -245,3 +245,4 @@ if not input_invalid:
     if not input_invalid:
         stars_display = "*" * num_stars
         # Lab 5 - Question 2: 
+        print("    |    Hero " + short_name + "gets <" + stars_display + "> start")

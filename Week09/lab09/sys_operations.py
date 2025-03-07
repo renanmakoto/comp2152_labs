b218366ca1984bc7e10b9bcf1a849e3fb82eac92
renan <renanmakoto@outlook.com>
2025-03-07 10:38:10 -0500

    Week 09 - Used platform and socket libs within sys_operations.py

--- Week09/lab09/sys_operations.py
+++ Week09/lab09/sys_operations.py
@@ -1,6 +1,7 @@
 # sys_operations.py
 import platform
 import socket
+import os
 
 print("Machine type: ")
 print(platform.machine())
@@ -11,3 +12,9 @@ print(platform.architecture())
 socket.setdefaulttimeout(50)
 print("Get the default Socket Timeout in seconds")
 print(socket.getdefaulttimeout())
+
+print("OS Type: ")
+print(os.name)
+
+print("OS Name: ")
+print(os.system())

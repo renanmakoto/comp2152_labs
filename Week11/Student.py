db141ac627bfec65d5ab081915105aaa8c60429a
renan <renanmakoto@outlook.com>
2025-03-21 16:04:31 -0400

    Week11

--- Week11/Student.py
+++ Week11/Student.py
@@ -1,18 +1,19 @@
 from Person import Person
 
 class Student(Person):
-    def __init__(self, p_name, p_age, p_height, p_program):
+    def __init__(self, p_name, p_age, p_height, p_major):
         super().__init__(p_name, p_age, p_height)
-        self.__program = p_program
+        self.__major = p_major
         print("Now this object is a object for a student")
 
     @property
-    def program(self):
-        return self.__program
+    def major(self):
+        return self.__major
     
     @program.setter
-    def program(self, new_program):
-        self.__program = new_program
+    def program(self, new_major):
+        self.__major = new_major
 
+print("This time it's a Student object")
 student1 = Student("Renan", 33, 1.67, "T177")
-print(f"Student name: {str(student1.name)}\nAge: {int(student1.age)}\nHeight: {float(student1.height)}\nProgram: {str(student1.program)}")
+print(f"Student name: {str(student1.name)}\nAge: {int(student1.age)}\nHeight: {float(student1.height)}\nProgram: {str(student1.major)}")

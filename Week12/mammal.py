09c602b21a022e7295fcf4ff482a9b617c823e38
renan <renanmakoto@outlook.com>
2025-03-28 10:17:20 -0400

    Week 12

--- Week12/mammal.py
+++ Week12/mammal.py
@@ -1,8 +1,10 @@
 from heart import Heart
 
 class Mammal:
-    def __init__():  # Aggregation through parameter
+    def __init__(self, p_age, tick=None):  # Aggregation through parameter
         print("Constructor: Inside the Parent class constructor: Making the Mammal part of the object")
+        self.age = p_age
+        self.__live_birth = True
 
     def __del__(self):
         print("Destructor: The garbage collector is now deleting the Mammal part of the object")
